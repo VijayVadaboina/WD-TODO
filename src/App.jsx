a5af137b8fce0eaf48f265659095baffae1cc952
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-10-08 09:19:54 +0530

    adding header component & styling it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Todo from "./components/Todo";
+import Header from "./components/Header";
+import "./App.css";
 function App() {
   return (
     <>
       <div>
+        <Header />
         <Todo />
       </div>
     </>
