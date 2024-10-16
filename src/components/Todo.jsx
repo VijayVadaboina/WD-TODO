08567c8b4d759c493653a253a7147495a06a64fa
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-10-16 19:04:10 +0530

    added count total todos and completed todos functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 import Form from "./Form";
+import Footer from "./Footer";
 import { TodoList } from "./TodoList";
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length;
   return (
     <div className="App">
       <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
     </div>
   );
 }
